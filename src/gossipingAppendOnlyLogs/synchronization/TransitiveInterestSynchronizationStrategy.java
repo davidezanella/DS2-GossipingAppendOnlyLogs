90e9e1b566cddaa1f93272fdfc2d59bf3684827b
Davide <davide.zanella10@gmail.com>
2020-12-09 11:34:43 +0100

    Improved Transitive Interest Synchronization

--- src/gossipingAppendOnlyLogs/synchronization/TransitiveInterestSynchronizationStrategy.java
+++ src/gossipingAppendOnlyLogs/synchronization/TransitiveInterestSynchronizationStrategy.java
@@ -7,21 +7,26 @@ import gossipingAppendOnlyLogs.models.Store;
 public class TransitiveInterestSynchronizationStrategy implements SynchronizationStrategy {
     @Override
     public void synchronize(Store localStore, Store remoteStore, PersonPublicKey localId, PersonPublicKey remoteId) {
-    	// add missing following log ids
-    	var followingIds = localStore.getLogsFollowedBy(localId);
-    	followingIds.removeAll(localStore.getIds());
-
-    	followingIds.stream().map(id -> new Log(id)).forEach(localStore::add);
-    	
-    	// remove blocked log ids
-    	var blockedIds = localStore.getLogsBlockedBy(localId);
-    	blockedIds.retainAll(localStore.getIds());
-    	
-    	blockedIds.stream().forEach(localStore::remove);
+    	createMissingLogs(localStore, localId);
+    	createMissingLogs(remoteStore, remoteId);
 
     	// we should now update the stores
     	var frontier = localStore.getFrontier(localStore.getIds());
     	var news = remoteStore.getEventsSince(frontier);
     	localStore.update(news);
     }
+
+	private void createMissingLogs(Store store, PersonPublicKey creatorId) {
+		// add missing following log ids
+    	var followingIds = store.getLogsFollowedBy(creatorId);
+    	followingIds.removeAll(store.getIds());
+
+    	followingIds.stream().map(id -> new Log(id)).forEach(store::add);
+    	
+    	// remove blocked log ids
+    	var blockedIds = store.getLogsBlockedBy(creatorId);
+    	blockedIds.retainAll(store.getIds());
+    	
+    	blockedIds.stream().forEach(store::remove);
+	}
 }
