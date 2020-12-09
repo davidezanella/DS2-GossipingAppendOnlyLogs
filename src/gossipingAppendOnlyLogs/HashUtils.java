eba35c91e34619501208ae8d401b9fc96235adec
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-12-09 11:15:54 +0200

    Implement hash function and event signing

--- src/gossipingAppendOnlyLogs/HashUtils.java
+++ src/gossipingAppendOnlyLogs/HashUtils.java
@@ -1,9 +1,19 @@
 package gossipingAppendOnlyLogs;
 
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class HashUtils {
 
-    public static String hash(String toHash) {
-        throw new RuntimeException("not implemented yet");
-    }
+	public static String hash(String toHash) {
+		try {
+			var digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(toHash.getBytes(StandardCharsets.UTF_8));
+			return new String(hash, StandardCharsets.UTF_8);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("cannot compute SHA-256 hash", e);
+		}
+	}
 }
--- src/gossipingAppendOnlyLogs/events/EventSignature.java
+++ src/gossipingAppendOnlyLogs/events/EventSignature.java
@@ -1,9 +1,19 @@
 package gossipingAppendOnlyLogs.events;
 
+import gossipingAppendOnlyLogs.CryptographyUtils;
 import gossipingAppendOnlyLogs.models.PersonKeys;
 
 public class EventSignature {
-    public static EventSignature of(Event event, PersonKeys keys) {
-        throw new RuntimeException("not implemented yet");
+
+	public final String signature;
+
+	public static EventSignature of(Event event, PersonKeys keys) {
+    	var description = event.getCreatorId().hash() + event.getPreviousEventHash() + event.hashContent();
+		var signature = CryptographyUtils.encrypt(keys.privateKey.key, description);
+		return new EventSignature(signature);
     }
+
+	public EventSignature(String signature) {
+		this.signature = signature;
+	}
 }
