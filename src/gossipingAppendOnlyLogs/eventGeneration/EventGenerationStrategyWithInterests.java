6d2d5c22b0aab3ced36195b1e97db6670f674226
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-12-13 06:59:55 +0200

    refactor EventGenerationStrategyWithInterests

--- src/gossipingAppendOnlyLogs/eventGeneration/EventGenerationStrategyWithInterests.java
+++ src/gossipingAppendOnlyLogs/eventGeneration/EventGenerationStrategyWithInterests.java
@@ -12,6 +12,7 @@ import repast.simphony.engine.schedule.ScheduleParameters;
 import repast.simphony.random.*;
 import gossipingAppendOnlyLogs.RepastUtils;
 
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,157 +25,118 @@ public class EventGenerationStrategyWithInterests extends EventGenerationStrateg
 		scheduler.schedule(params, this, "generateInterestEvent");
 	}
 
-	/*every now and then, create a stream event (this makes sure that a person actively 
-	 *posts new events and doesn't only follow/unfollow other members of the system 
+	/*every now and then, create a stream event (this makes sure that a person actively
+	 *posts new events and doesn't only follow/unfollow other members of the system
 	 */
 	@Override
 	protected void generateEvent() {
 		var event = new StreamEvent("Some content");
 		person.addEventToPersonalLog(event);
 	}
-	
-	
+
+
 	protected void generateInterestEvent() {
-		//make a follow, unfollow, block or unblock event
-		//in order to make this more realistic these events don't have all the same probabilites
-		int eventChosen = RandomHelper.nextIntFromTo(0, 10);
-		Boolean eventDone = false;
-		
-		while(!eventDone) {
-			Event event = null;
-			if(eventChosen <= 4)
+		// TODO: can we be stuck in a infinite loop?
+		Event event;
+		do {
+			int choice = RandomHelper.nextIntFromTo(0, 10);
+			if (choice <= 4) {
 				//a followEvent is the most probable event type
 				event = createFollowEvent();
-			else if (eventChosen <= 7) 
+			} else if (choice <= 7) {
 				event = createUnfollowEvent();
-			else if(eventChosen <= 8)
+			} else if (choice <= 8) {
 				event = createBlockEvent();
-			else {
+			} else {
 				event = createUnblockEvent();
 			}
-			
-			if(event != null) {
-				person.addEventToPersonalLog(event);
-				eventDone = true;
-			}
-			
-			//in the case that an event wasn't complete (e.g. unblock event when the person never
-			//blocked someone, revert the event to a follow event
-			eventChosen = 0;
-		}
+		} while (event == null);
+		person.addEventToPersonalLog(event);
 	}
-	
+
 	protected FollowEvent createFollowEvent() {
-		PersonPublicKey persToFollow = null;
-		
-		//get a set with the pkeys of everyone
-		Set<PersonPublicKey> persons = RepastUtils
-	        .getAllPeopleInGrid(person)
-	        .stream()
-	        .map(availPers -> availPers.getPublicKey())
-	        .collect(Collectors.toSet());
-		
-		//set with all already followed persons
-		Set <PersonPublicKey> following = person
-			.getStore()
-			.getLogsFollowedBy(person.getPublicKey());
-		
-		Set <PersonPublicKey> blocked = person
-			.getStore()
-			.getLogsBlockedBy(person.getPublicKey());
-		
+		var people = getAllPeople();
+
+		var alreadyFollowing = person
+				.getStore()
+				.getLogsFollowedBy(person.getPublicKey());
+
+		var blocked = person
+				.getStore()
+				.getLogsBlockedBy(person.getPublicKey());
+
 		//I don't want to start following persons Im already following or have blocked
-		persons.removeAll(following);
-		persons.removeAll(blocked);
-		
-		int item = RandomHelper.nextIntFromTo(0, persons.size()); // In real life, the Random object should be rather more shared than this
-		int i = 0;
-		
-		for(PersonPublicKey key : persons)
-		{
-		    if (i == item)
-		        persToFollow = key;
-		    i++;
+		people.removeAll(alreadyFollowing);
+		people.removeAll(blocked);
+
+		if (people.isEmpty()) {
+			return null;
 		}
-		
-		return new FollowEvent(persToFollow);
+		return new FollowEvent(pickRandomPerson(people));
 	}
-	
+
 	protected UnfollowEvent createUnfollowEvent() {
-		PersonPublicKey persToUnfollow = null;
-		
-		//set with all already followed persons
-		Set <PersonPublicKey> following = person
-			.getStore()
-			.getLogsFollowedBy(person.getPublicKey());
-		
+		var following = person
+				.getStore()
+				.getLogsFollowedBy(person.getPublicKey());
+
 		// TODO: can I follow a person I've blocked?
-		Set <PersonPublicKey> blocked = person
-			.getStore()
-			.getLogsBlockedBy(person.getPublicKey());
-		
+		Set<PersonPublicKey> blocked = person
+				.getStore()
+				.getLogsBlockedBy(person.getPublicKey());
+
 		following.removeAll(blocked);
-		
-		int item = RandomHelper.nextIntFromTo(0, following.size()); // In real life, the Random object should be rather more shared than this
-		int i = 0;
-		
-		for(PersonPublicKey key : following)
-		{
-		    if (i == item)
-		        persToUnfollow = key;
-		    i++;
+
+		if (following.isEmpty()) {
+			return null;
 		}
-		
-		return new UnfollowEvent(persToUnfollow);
+
+		return new UnfollowEvent(pickRandomPerson(following));
 	}
-	
+
 	protected BlockEvent createBlockEvent() {
-		PersonPublicKey persToBlock = null;
-		
-		//get a set with the pkeys of everyone
-		Set<PersonPublicKey> persons = RepastUtils
-	        .getAllPeopleInGrid(person)
-	        .stream()
-	        .map(availPers -> availPers.getPublicKey())
-	        .collect(Collectors.toSet());
-		
-		Set <PersonPublicKey> blocked = person
-			.getStore()
-			.getLogsBlockedBy(person.getPublicKey());
-		
-		persons.removeAll(blocked);
-		
-		int item = RandomHelper.nextIntFromTo(0, persons.size()); // In real life, the Random object should be rather more shared than this
-		int i = 0;
-		
-		for(PersonPublicKey key : persons)
-		{
-		    if (i == item)
-		    	persToBlock = key;
-		    i++;
+		var blocked = person
+				.getStore()
+				.getLogsBlockedBy(person.getPublicKey());
+
+		var blockablePeople = getAllPeople()
+				.stream()
+				.filter(person -> !blocked.contains(person))
+				.collect(Collectors.toSet());
+
+		if (blockablePeople.isEmpty()) {
+			return null;
 		}
-		
-		return new BlockEvent(persToBlock);
+
+		return new BlockEvent(pickRandomPerson(blockablePeople));
 	}
-	
+
+	private Set<PersonPublicKey> getAllPeople() {
+		return RepastUtils
+				.getAllPeopleInGrid(person)
+				.stream()
+				.map(Person::getPublicKey)
+				.collect(Collectors.toSet());
+	}
+
 	protected UnblockEvent createUnblockEvent() {
-		PersonPublicKey persToUnblock = null;
-		
-		Set <PersonPublicKey> blocked = person
-			.getStore()
-			.getLogsBlockedBy(person.getPublicKey());
-		
-		int item = RandomHelper.nextIntFromTo(0, blocked.size()); // In real life, the Random object should be rather more shared than this
-		int i = 0;
-		
-		for(PersonPublicKey key : blocked)
-		{
-		    if (i == item)
-		    	persToUnblock = key;
-		    i++;
+		Set<PersonPublicKey> blocked = person
+				.getStore()
+				.getLogsBlockedBy(person.getPublicKey());
+
+		if (blocked.isEmpty()) {
+			return null;
 		}
-		
-		return new UnblockEvent(persToUnblock);
+
+		return new UnblockEvent(pickRandomPerson(blocked));
+	}
+
+	private static PersonPublicKey pickRandomPerson(Set<PersonPublicKey> persons) {
+		if (persons.isEmpty()) {
+			throw new IllegalArgumentException("persons set is empty");
+		}
+		int item = RandomHelper.nextIntFromTo(0, persons.size());
+		return new ArrayList<>(persons).get(item);
 	}
 }
 
