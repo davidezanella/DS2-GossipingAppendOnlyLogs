64c500044688afef127250e0393be85130912a79
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-12-14 20:26:27 +0200

    move test in the directory used by Eclipse

---
+++ test/gossipingAppendOnlyLogs/CanaryTest.java
@@ -0,0 +1,14 @@
+package gossipingAppendOnlyLogs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CanaryTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
