5fabf60b9321e179e7ce74a255cc486d228a8ef3
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-12-31 19:50:30 +0100

    make generate_batch_3d more generic

--- generate_batch_3d.py
+++ generate_batch_3d.py
@@ -1,26 +1,32 @@
 import os
+import xml.etree.ElementTree as ET
 
 protocols = ["Open", "Transitive"]
+
 
 def main():
     repeat_run = 12
 
-    lan_values = [i for i in range(3, 20 + 1, 2)]
-    people_values = [i for i in range(10, 50 + 1, 10)]
+    x_variable = 'numPeople'
+    x_values = [i for i in range(5, 25 + 1, 5)]
 
-    count = 1000
-    for x in lan_values:
-        for y in people_values:
+    y_variable = 'meanPrefLANs'
+    y_values = [i for i in range(1, 10 + 1, 2)]
+
+    count = 2000
+    for x in x_values:
+        for y in y_values:
             for protocol in protocols:
-                generate_config(count, protocol, x, y, repeat_run)
+                generate_config(count, protocol, x_variable, x, y_variable, y, repeat_run)
             count += 1
 
 
-def generate_config(config_number: int, protocol: str, lan: int, people: int, repeat_run: int):
+def generate_config(config_number: int, protocol: str, x_variable: str, x: int, y_variable: str, y: int,
+                    repeat_run: int):
     folder = f"batch/{config_number}"
     os.makedirs(folder, exist_ok=True)
     generate_properties(folder, protocol, config_number)
-    generate_xml(folder, protocol, lan, people, repeat_run)
+    generate_xml(folder, protocol, x_variable, x, y_variable, y, repeat_run)
 
 
 def generate_properties(folder: str, protocol: str, config_number: int):
@@ -40,35 +46,23 @@ vm.arguments=
         f.write(properties)
 
 
-def generate_xml(folder: str, protocol: str, lan: int, people: int, repeat_run: int):
-    xml = f"""<?xml version="1.0" ?>
-<sweep runs="{str(repeat_run)}">
-    <parameter name="stopAt" type="constant" constant_type="int"
-        value="1000"></parameter>
-    <parameter name="numLANs" type="constant" constant_type="int"
-        value="{str(lan)}"></parameter>
-    <parameter name="meanTicksWaiting" type="constant"
-        constant_type="int" value="5"></parameter>
-    <parameter name="numPeople" type="constant"
-        constant_type="int" value="{str(people)}"></parameter>
-    <parameter name="meanPrefLANs" type="constant"
-        constant_type="int" value="1"></parameter>
-    <parameter name="stdTicksWaiting" type="constant"
-        constant_type="int" value="5"></parameter>
-    <parameter name="stdPrefLANs" type="constant"
-        constant_type="int" value="1"></parameter>
-    <parameter name="motionStrategy" type="constant"
-        constant_type="java.lang.String" value="HabitMotion"></parameter>
-    <parameter name="synchronizationProtocol" type="constant"
-        constant_type="java.lang.String" value="{protocol}Model"></parameter>
-    <parameter name="meanTicksWaitingHome" type="constant"
-               constant_type="int" value="5"></parameter>
-    <parameter name="stdTicksWaitingHome" type="constant"
-               constant_type="int" value="2"></parameter>
-</sweep>
-    """
-    with open(f"{folder}/{protocol}.xml", "w") as f:
-        f.write(xml)
+def generate_xml(folder: str, protocol: str, x_variable: str, x: int, y_variable: str, y: int, repeat_run: int):
+    tree = ET.parse('template.xml')
+    root = tree.getroot()
+
+    root.set('runs', str(repeat_run))
+
+    update_value(root, 'synchronizationProtocol', f"{protocol}Model")
+    update_value(root, x_variable, str(x))
+    update_value(root, y_variable, str(y))
+
+    tree.write(f"{folder}/{protocol}.xml")
+
+
+def update_value(root, variable, value):
+    for child in root:
+        if child.attrib['name'] == variable:
+            child.set('value', value)
 
 
 if __name__ == '__main__':
